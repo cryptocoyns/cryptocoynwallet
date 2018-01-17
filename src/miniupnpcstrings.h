8b53818ddfa6d82637f3161813da2c76134b22d4
CryptoCoyns <33798562+cryptocoyns@users.noreply.github.com>
2018-01-17 16:57:27 +1000

    Update miniupnpcstrings.h

--- src/miniupnpcstrings.h
+++ src/miniupnpcstrings.h
@@ -1,3 +1,4 @@
+// Coypright (c) 2017 CryptoCoyns.com
 // Copyright (c) 2011-2015 The Cryptonote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -6,6 +7,6 @@
 #define MINIUPNPCSTRINGS_H_INCLUDED
 
 #define OS_STRING "Windows"
-#define MINIUPNPC_VERSION_STRING "1.9"
+#define MINIUPNPC_VERSION_STRING "1.0"
 
 #endif
